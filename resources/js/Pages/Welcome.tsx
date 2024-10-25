993b1bd8ff3d7b44ad864f2d176dc3ebc9835948
Adel <kovacsadel12@gmail.com>
2024-10-25 21:59:36 +0200

    add logs

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,7 +1,7 @@
 import { PageProps } from "@/types";
 import { Head } from "@inertiajs/react";
-import { SidebarProvider, SidebarTrigger } from "@/Components/ui/sidebar";
-import { AppSidebar } from "@/Components/AppSidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
 import { Home, Inbox, Settings } from "lucide-react";
 
 export default function Welcome({ ...props }: PageProps<any>) {
